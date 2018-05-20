18744150800083e7dc4316e7b9fcc5314773c46b
guruyaya <guruyaya@gmail.com>
2018-05-20 22:21:15 +0300

    Cleanup

--- js/chrome/test.js
+++ js/chrome/test.js
@@ -1,7 +1,6 @@
 (function(){
+  // get server pace2pace url
   readPace2PaceSignaturesDoc('http://www.balibalic.info/pace2pace.txt', function(){
     console.log('I am here');
   });
-  readPace2PaceSignaturesDoc('http://www.balibalic.info/Pace2PaceDocSigException.txt');
-  readPace2PaceSignaturesDoc('http://www.balibalic.info/Pace2PaceUrlInvalidException.txt');
 })();
